18adce488bbb07cf150940e9f5a4063789a0c187
GNandhan <gowrinandhan95@gmail.com>
2025-10-13 12:07:48 +0530

    Admin panel update

--- Admin/pages/admin-complaint.php
+++ Admin/pages/admin-complaint.php
@@ -14,7 +14,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Admin-Home</title>
   <link rel="stylesheet" href="../static/admin.css">
-  <link rel="icon" href="../static/icon.png">
+  <link rel="icon" href="../static/logo.png">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
--- Admin/pages/admin-login.php
+++ Admin/pages/admin-login.php
@@ -1,6 +1,6 @@
 <?php
  include './connect.php';
- error_reporting(0);
+//  error_reporting(0);
  session_start();
  $_SESSION["email"]='';
 ?>
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crime: Admin-Login</title>
     <link rel="stylesheet" href="User/static/home.css">
-    <link rel="icon" href="../static/icon.png">
+    <link rel="icon" href="../static/logo.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
